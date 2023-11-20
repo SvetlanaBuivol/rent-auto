1ffdda012603326670fd6d445099b1a311fc4e07
SvetlanaBuuvol <divinesvetok@gmail.com>
2023-11-20 23:19:58 +0200

    home page styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React from 'react';
+import {
+  HeaderTitle,
+  HomeContainer,
+  StyledText,
+  StyledText2,
+} from './Home.styled';
 
 const Home = () => {
-    
-    return (
-        <div>Home</div>
-    )
-}
+  return (
+    <HomeContainer>
+      <HeaderTitle>Drive the Experience. Rent Your Journey.</HeaderTitle>
+      <StyledText>
+        Unleash the road trip of your dreams with RentAuto.
+        <br /> Rent a car seamlessly, explore effortlessly.
+      </StyledText>
+      <StyledText2>Your adventure starts here.</StyledText2>
+    </HomeContainer>
+  );
+};
 
 export default Home;
